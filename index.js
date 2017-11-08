99fbd5000115286868048d4ee609cd17c430c802
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-08 23:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    let headquarters=42
+    let distances=pickup-headquarters
+    distances = Math.abs(distances);
+    return distances
+
+}
+function calculateVertical(){
+
+}
